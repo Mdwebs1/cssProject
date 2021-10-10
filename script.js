fb3c2c88715437e910f33e68112922a52757b5c6
Masha Aldosari <md.webs95@gmail.com>
2021-10-10 09:00:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+ 
+
+ function myFunction(){
+  var x = document.getElementsByClassName("mashaInside")[0];
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+  
+}
+
+
+
+
+
+
+
